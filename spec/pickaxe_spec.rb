f3fc786ca52eee252dad9aa5d36cd78c71bb9100
İsmail Arılık <arilik.ismail@gmail.com>
2023-05-07 15:34:43 +0300

    test(pickaxe): remove unnecessary test case

--- spec/pickaxe_spec.rb
+++ spec/pickaxe_spec.rb
@@ -4,9 +4,5 @@ RSpec.describe Pickaxe do
   it "has a version number" do
     expect(Pickaxe::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
 
